8c4c30e71826b56f07f794e535c0e175d800986a
Dan Markham <dmarkham@eventful-inc.com>
2015-08-21 07:07:45 +0000

    wip

--- hexseeker/hexseeker.go
+++ hexseeker/hexseeker.go
@@ -6,14 +6,3 @@ import (
 
 type Hexer struct{ io.ReadSeeker }
 
-func (h *Hexer) Read(data []byte) (n int, err error) {
-	return h.Read(data)
-}
-
-func (h *Hexer) Seek(offset int64, whence int) (int64, error) {
-	return h.Seek(offset, whence)
-}
-
-func NewHexer(rws io.ReadSeeker) io.ReadSeeker {
-	return &Hexer{rws}
-}
