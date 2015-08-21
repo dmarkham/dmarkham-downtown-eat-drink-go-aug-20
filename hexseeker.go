c7e69adabec994f4a85e4890c82142b7006b362a
Dan Markham <dmarkham@eventful-inc.com>
2015-08-20 23:27:23 -0700

    playing

---
+++ hexseeker.go
@@ -0,0 +1,16 @@
+package hexseeker
+
+
+var Hexer io.ReadSeeker
+
+func (h *Hexer) Read(data []byte) (n int, err error) {
+   return h.read(data)
+}
+
+func (h *Hexer) Seek(offset int64, whence int) (int64, error){
+  return h.Seek(offset,whence)
+}
+
+type func NewHexer(rws io.ReadSeeker) (io.ReadSeeker){
+   return &Hexer{rws} 
+}
