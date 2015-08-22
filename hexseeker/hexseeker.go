18858770380d7a8dfe628d66d205d3c088e1ebbd
Dan Markham <dmarkham@eventful-inc.com>
2015-08-22 01:33:18 +0000

    wip

--- hexseeker/hexseeker.go
+++ hexseeker/hexseeker.go
@@ -1,28 +1,32 @@
 package hexseeker
 
 import (
+	"encoding/hex"
 	"io"
-  "encoding/hex"
 )
 
 type Hexer struct{ io.ReadSeeker }
 
 func NewHexer(rws io.ReadSeeker) *Hexer {
-       return &Hexer{rws}
+	return &Hexer{rws}
 }
 
-func (h *Hexer) FmtAt(offset, n int64) (string){
-  _, err :=   h.Seek(offset,0)
-  if err != nil{
-    return ""
-  }
-  var buf = make([]byte, n)
-  _ , err = h.Read(buf)  
-  
-  if err != nil{
-    return ""
-  }
-  st := hex.Dump(buf)
-  return st
-}
+func (h *Hexer) FmtAt(offset, n int64) string {
+	_, err := h.Seek(offset, 0)
+	if err != nil {
+		return ""
+	}
+	var buf = make([]byte, n)
+	_, err = h.Read(buf)
 
+	if err != nil {
+		return ""
+	}
+
+	st := ""
+	for i := 0; i < int(n/16)+1; i++ {
+		stT := hex.Dump(buf[i:16])
+		st = st + stT
+	}
+	return st
+}
